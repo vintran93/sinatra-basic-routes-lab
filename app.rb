b5bf49388b39b25c1f9c8a4b98d7d8f2e432a952
vincent <vincentuong@yahoo.com>
2021-08-29 15:35:59 -0500

    commit tree 9a790d50efbd09acc0c04a0239e3c36577cb70fd

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Vincent."
+    end
+
+    get '/hometown' do
+        "My hometown is Lewisville."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'As I am'."
+    end
 end
